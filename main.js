eba290f1293876e011f2c17f9ab879bf8eb5cc55
Joshua Benuck <joshua_benuck@yahoo.com>
2019-03-30 08:06:15 -0700

    Added basic CLI for debugging and starting a timer.

--- main.js
+++ main.js
@@ -7,53 +7,50 @@ const joinCuckoo = () => {
 
 joinCuckoo()
 
-io.on("connection", (socket) => {
-	console.log({connected: true, socket});
-});
+const CONCISE = "concise"
+const ALL = {depth: null}
+
+const debug = (name, e, depth) => {
+	if (depth == CONCISE) {
+		console.log(name)
+		return
+	}
+	obj = {}
+	obj[name] = true
+	obj["e"] = e
+	console.dir(obj, depth);
+}
+
+const TIMER_FINISHED = "finish-timer"
+const events = [
+	"connection",
+	"ping",
+	"update timer",
+	TIMER_FINISHED,
+	"update user",
+	"update users",
+	"update sessions",
+	"update settings",
+	"update activities",
+	"update activity",
+	"validation error",
+	"user joined",
+	"update roadmap"
+]
+
+const debug_event = (event, depth) => {
+	console.log(`Registering listenter for the ${event} event.`)
+	io.on(event, (e) => { debug(event, e, depth) })
+}
+
+const debug_all = (depth) => {
+	events.forEach((event) => {
+		debug_event(event, depth)
+	})
+}
 
 io.on("ping", (e) => {
-	console.log({ping: true, e});
 	io.emit("pong");
-});
-
-io.on("update timer", (e) => {
-	console.log({updateTimer: true, e});
-});
-
-io.on("finish timer", (e) => {
-	console.log({finishTimer: true, e});
-});
-
-io.on("update user", (e) => {
-	console.log({updateUser: true, e});
-});
-
-io.on("update users", (e) => {
-	console.log({updateUsers: true, e});
-});
-
-io.on("update sessions", (e) => {
-	console.log({updateSessions: true, e});
-});
-
-io.on("update settings", (e) => {
-	console.log({updateSettings: true, e});
-});
-
-io.on("update activity", (e) => {
-	console.log({updateActivity: true, e});
-});
-
-io.on("validation error", (e) => {
-	console.log({validationError: true, e});
-});
-
-io.on("user joined", (e) => {
-	console.log({userJoined: true, e});
-});
-
-io.on("update roadmap", (e) => {
-	console.log({updateRoadmap: true, e});
 });
 
 const skipSessionType = () => {
@@ -74,5 +71,61 @@ const createRoadmapSession = (e) => {
 
 const clearRoadmap = (e) => {
 	io.emit("clear roadmap")
+}
+
+const describeDebugOptions = (yarg) => {
+	events.forEach((event) => {
+		let e = event.replace(" ", "-")
+		yarg.describe(`debug-${e}`, `Registers listener for the ${event} event.`)
+	})
+	return yarg
+}
+
+yarg = require("yargs")
+argv = describeDebugOptions(yarg)
+	.describe("debug-all", "Register listeners for all events.")
+	.alias("level", "l")
+	.describe("level", "Sets the level to log at.")
+	.choices("level", ["all", "concise"])
+	.describe("break <x>", "Start a break for <x> minutes.")
+	.describe("work <x>", "Start a break for <x> minutes.")
+	.parse()
+if (argv.level == "all") argv.level=ALL
+if (argv.level == "concise") argv.level=CONCISE
+
+if (argv["debug-all"]) {
+	debug_all(argv.level)
+}
+
+for (let event of Object.keys(argv)) {
+	if (event.startsWith("debug-")
+		&& event.indexOf("all") == -1) {
+		debug_event(event.split("debug-")[1].replace("-", " "), argv.level)
+	}
+}
+
+const startTimer = (type, duration) => {
+	let sessionType = null
+	let waitingForSkip = false
+	let desiredType = type
+	listener = (e) => {
+		if (!waitingForSkip && sessionType != desiredType) {
+			waitingForSkip = true
+			skipSessionType()
+			return
+		}
+		console.log(`Started ${type} timer for ${duration} minutes.`)
+		io.emit("start timer", duration)
+		io.off("update activity", listener)
+	}
+	io.on("update activity", listener)
+}
+
+if (argv["work"]) {
+	startTimer("work", argv["work"])
+}
+
+if (argv["break"]) {
+	startTimer("breakTime", argv["break"])
 }
 
